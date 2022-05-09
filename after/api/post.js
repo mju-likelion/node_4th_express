e1309b8acd2c7d68636710a9504b6ce6191f3e92
minjung <ypd06021@naver.com>
2022-05-09 18:07:24 +0900

    오타 수정 및 주석 추가

--- after/api/post.js
+++ after/api/post.js
@@ -3,11 +3,14 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/", (req, res) => {
-  const text = req.body;
+  //request의 body 안 text를 text라는 변수에 할당
+  const text = req.body.text;
 
+  // response code 200번과 함께 json 형태(아래의 data)를 응답
   return res.status(200).json({
     data: {
-      message: "POST" + text,
+      message: "POST",
+      text,
     },
   });
 });
--- after/index.js
+++ after/index.js
@@ -5,8 +5,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use("./api", api);
+app.use("/api", api);
 
 app.listen(port, () => {
-  console.log(`이걸 안와 ? http://localhost:${port}`);
+  console.log(`서버실행 => http://localhost:${port}`);
 });
