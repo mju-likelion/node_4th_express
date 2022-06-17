fb7b402c8cfe45d6977577c2bb016657651c46ed
minjung <ypd06021@naver.com>
2022-06-18 04:22:00 +0900

    cors 테스트용 get 라우터 추가

--- after/api/get.js
+++ after/api/get.js
@@ -2,8 +2,20 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("get");
+router.get("/1", (req, res) => {
+  return res.status(200).json({
+    data: {
+      res: "get-1",
+    },
+  });
+});
+
+router.get("/2", (req, res) => {
+  return res.status(200).json({
+    data: {
+      res: "get-2",
+    },
+  });
 });
 
 export default router;
